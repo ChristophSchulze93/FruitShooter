fe32b0fc5b511917b84fcfee1fe6b9c335b21533
Christoph Schulze <christophschulze93@gmail.com>
2023-05-23 15:32:45 +0200

    added lose screen, fixed point bug, changed fruit-spawning, added restart button

--- Assets/Scripts/FruitSpawner.cs
+++ Assets/Scripts/FruitSpawner.cs
@@ -17,6 +17,10 @@ public class FruitSpawner : MonoBehaviour
     [SerializeField]
     private int m_SpawnRange = 20;
 
+    public int spawnSearchedFruitInterval = 2;
+
+    private int m_currentSearchedFruitIntervalCounter = 0;
+
     private IEnumerator SpawnRandomFruit()
     {
         yield return new WaitForSeconds(m_spawnWaitingTime);
@@ -28,7 +32,7 @@ public class FruitSpawner : MonoBehaviour
     {
         while (m_GameRunning)
         {
-            yield return SpawnRandomFruit();
+            yield return SpawnFruit();
         }
     }
 
@@ -36,6 +40,30 @@ public class FruitSpawner : MonoBehaviour
     {
         return fruitList[Random.Range(0, fruitList.Count)];
     }
+
+
+    public IEnumerator SpawnFruit()
+    {
+        yield return new WaitForSeconds(m_spawnWaitingTime);
+        float randomXPosition = Random.Range(spawnPosition.position.x - m_SpawnRange, spawnPosition.position.x + m_SpawnRange);
+
+
+
+        if (spawnSearchedFruitInterval == m_currentSearchedFruitIntervalCounter)
+        {
+            Instantiate(m_FruitList[GameMode.Instance.fruitTargetIndex], new Vector3(randomXPosition, spawnPosition.position.y, spawnPosition.position.z), Quaternion.identity);
+            m_currentSearchedFruitIntervalCounter = 0;
+        }
+        else
+        {
+            Instantiate(PickRandomFruitFromList(m_FruitList), new Vector3(randomXPosition, spawnPosition.position.y, spawnPosition.position.z), Quaternion.identity);
+
+        }
+
+        m_currentSearchedFruitIntervalCounter++;
+    }
+
+
 
 
 }
